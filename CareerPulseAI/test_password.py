b812953027766ee686812e034b56406f4fb0debd
Priyanshu Kumar <149371231+priyanshu7289@users.noreply.github.com>
2026-07-09 14:28:02 +0530

    Delete CareerPulseAI/test_password.py

--- CareerPulseAI/test_password.py
+++
@@ -1,9 +0,0 @@
-from auth.password import hash_password, verify_password
-
-password = "Priyanshu123"
-
-hashed = hash_password(password)
-
-print(hashed)
-
-print(verify_password(password, hashed))
